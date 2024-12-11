304323793549339be349aa404052c2a77e7c7712
MaartenVerm <maarten.vermeulen@hotmail.nl>
2024-12-11 10:51:01 +0100

    Test test

---
+++ chartjs-population/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <ProductVerkopenChart />
+</template>
+
+<script>
+import ProductVerkopenChart from './components/ProductVerkopenChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+     ProductVerkopenChart }
+  
+  }
+
+</script>
+
+<style>
+
+</style>
